6eac34b1b1335ea76c0dc1b7a734702b7eb38820
Seohyun Lee <cwijiq3085@gmail.com>
2025-05-09 22:33:01 +0900

    main page default setting

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from '../partials/Header';
+import Banner from '../partials/Banner';
+
+function SignUp() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="grow">
+
+        <section className="relative">
+          <div className="mx-auto px-4 sm:px-6">
+            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+              {/* Page header */}
+              <div className="w-[38%] mx-auto text-left pb-12 md:pb-20">
+                <h1 className="h2">텃밭마켓와 함께 <br /> 내 농산물 판매를 시작하세요</h1>
+              </div>
+
+              {/* Form */}
+              <div className="w-[30%] mx-auto">
+                <form>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="name">이름 <span className="text-red-600">*</span></label>
+                      <input id="name" type="text" className="form-input w-full text-gray-300" placeholder="이름을 입력하세요" required />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="email">이메일 <span className="text-red-600">*</span></label>
+                      <input id="email" type="email" className="form-input w-full text-gray-300" placeholder="이메일 주소" required />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="password">비밀번호 <span className="text-red-600">*</span></label>
+                      <input id="password" type="password" className="form-input w-full text-gray-300" placeholder="비밀번호" required />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="password">비밀번호 확인 <span className="text-red-600">*</span></label>
+                      <input id="password" type="password" className="form-input w-full text-gray-300" placeholder="비밀번호 확인" required />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <div className="flex justify-between">
+                        <label className="flex items-center">
+                          <input type="checkbox" className="form-checkbox" />
+                          <span className="text-gray-300 ml-2">개인정보 수집 및 이용에 동의합니다.</span>
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mt-6">
+                    <div className="w-full px-3">
+                      <button className="btn text-white bg-[#688A08] hover:bg-[#688A08]/80 w-full">회원가입</button>
+                    </div>
+                  </div>
+                </form>
+                <div className="text-gray-300 text-center mt-6">
+                  Already using Open PRO? <Link to="/signin" className="text-gray-600 hover:underline" href="#0">Sign in</Link>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+
+      </main>
+
+    </div>
+  );
+}
+
+export default SignUp;
